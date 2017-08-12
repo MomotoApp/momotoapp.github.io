c8f53aca7a15a61d98c016ddd912709651bc795c
Won Yong Ha <hpmsora@gmail.com>
2017-08-11 23:57:48 -0400

    Footer added

--- JAVASCRIPT/MainJavascript.js
+++ JAVASCRIPT/MainJavascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('#MainNavigation_id').load("SECTIONS/MainNavigation.php");
     $('#MainContent_id').load('SECTIONS/AboutContents.php');
-    $('#MainFooterWrap_id').load('SECTIONS/FooterContents.php');
+    $('#MainFooterWrap_id').load('SECTIONS/Footer.php');
 });
---
+++ SECTIONS/Footer.php
@@ -0,0 +1,11 @@
+<div class="FooterWrap">
+  <span>Copyright &copy; Redym Ltd</span>
+  <span>
+    <a href="#"><img src="ASSETS/Facebook_Icon.png" alt="Facebook Profile" height="20px" width="20px"></a>
+    <a href="#"><img src="ASSETS/Instagram_Icon.png" alt="Instagram Profile" height="20px" width="20px"></a>
+    <a href="#"><img src="ASSETS/Linkedin_Icon.png" alt="LinkedIn Profile" height="20px" width="20px"></a>
+    <a href="https://github.com/MomotoApp"><img src="ASSETS/Github_Icon.png" alt="Github Profile" height="20px" width="20px"></a>
+  </span>
+</div>
+
+<link rel="stylesheet" type="text/css" href="CSS/FooterStyle.css"/>
