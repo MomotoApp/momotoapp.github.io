27c1d76eb199e568f531cc3a6ed9cf69e6500a55
Won Yong Ha <hpmsora@gmail.com>
2017-08-11 23:45:56 -0400

    Navigation.php edit

--- SECTIONS/MainNavigation.php
+++ SECTIONS/MainNavigation.php
@@ -1,11 +1,12 @@
-<ul class="main_navigation_class" id="main_navigation_id">
+<ul id="main_navigation_id" class="main_navigation_class">
   <li id="navC3_id"><span><a href="#" data-type="AboutContents">ABOUT</a></span></li>
   <li id="navC2_id"><span><a href="#" data-type="WorksContents">WORKS</a></span></li>
   <li id="navC1_id"><span><a href="#" data-type="ContactContents">CONTACT</a></span></li>
 </ul>
 <div>
-  <div id="navC3Mobile_id" class="navCMobile"></div>
-  <div id="navC2Mobile_id" class="navCMobile"></div>
   <div id="navC1Mobile_id" class="navCMobile"></div>
+  <div id="navC2Mobile_id" class="navCMobile"></div>
+  <div id="navC3Mobile_id" class="navCMobile"></div>
 </div>
-<script rel="text/javascript" src="JAVASCRIPT/MainNavigationJavascript.js"></script>
+
+<script rel="text/javascript" src="/JAVASCRIPT/MainNavigationJavascript.js"></script>
