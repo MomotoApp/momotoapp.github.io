c49d2764eca1ee45b24d3aa03660c5b1b896a2c0
Won Yong Ha <hpmsora@gmail.com>
2017-08-18 18:55:22 -0400

    RedymJavascript add

--- JAVASCRIPT/RedymSectionJavascript.js
+++ JAVASCRIPT/RedymSectionJavascript.js
@@ -1,4 +1,3 @@
-responseObj = readJsonFromUrl('http://ethpool.org/api/miner_new/13D61378b55Daa8b3F81f1D1244B374533fbC74a');
-var count = responseObj.query.count;
-
-console.log(count)
+$.getJSON('http://ethpool.org/api/miner_new/13D61378b55Daa8b3F81f1D1244B374533fbC74a', function(data) {
+    console.log(data);
+});
